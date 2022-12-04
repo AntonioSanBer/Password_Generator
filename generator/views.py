be539ca24b71958f9a49ec9fae35d9f42faf05b9
AntonioSanBer <antonio.asb94@gmail.com>
2022-12-04 11:33:36 +0100

    Password Generator

---
+++ generator/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random 
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+
+def password(request):
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    generated_password = ' '
+
+    
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('"$/()!^-_+&.'))
+    
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+    
+    length = int(request.GET.get('length'))
+
+    for char in range(length):
+        generated_password += random.choice(characters)
+
+
+    return render(request, 'password.html', {'password':generated_password})
